a49b5807d420a4cb54d5eef67fa006401fb21d22
Hossein.Gholami <h.reza73327@gmail.com>
2022-05-28 10:11:13 +0430

    make sign work as described in api doc.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import json
 from pprint import pprint
+import random
 
 load_dotenv()
 
@@ -15,19 +16,36 @@ d = Dextrade(key=key, secret=secret)
 
 # Test private commands
 a = d(PrivateCommands.ACTIVEORDERS_, args={})
-
-# args = {
-#     "type_trade": OrderReqTypeTrade.LIMIT.value,
-#     "type": OrderReqType.BUY.value,
-#     "rate": 1.1,
-#     "stop_rate": 0,
-#     "volume": 10,
-#     "pair": "CYRUSUSDT"
-# }
-# a = d(PrivateCommands.CREATEORDER_, args=args)
-
 print(a.status_code)
 print(a.text)
+
+balance = {"USDT":0, "CYRUS":0}
+b = d(PrivateCommands.BALANCES_, args={})
+b = b.json()["data"]["list"]
+print("Available balances: ")
+for i,_ in enumerate(b):
+    if _["currency"]["iso3"] == "USDT":
+        balance["USDT"] = _["balance_available"]*1E-8
+    if _["currency"]["iso3"] == "CYRUS":
+        print(_)
+        balance["CYRUS"] = _["balance_available"]*1E-8
+# get available balance
+print(balance)
+
+print("Sending order in progress...")
+args = {
+    "type_trade": OrderReqTypeTrade.LIMIT.value,
+    "type": OrderReqType.BUY.value,
+    "rate": 1.45361540,
+    "stop_rate": 0,
+    "volume": round(random.uniform(0.3*balance["USDT"], 0.7*balance["USDT"]),2),
+    "pair": "CYRUSUSDT"
+}
+a = d(PrivateCommands.CREATEORDER_, args=args)
+print(a.status_code)
+print(a.text)
+a = a.json()
+
 
 # Test public commands
 # a = d(PublicCommands.SYMBOLS_)
